0572313149629be632ecba9d15ad8770107daa3c
Hallow_Tommy <151835761+HallowTommy@users.noreply.github.com>
2026-03-27 11:11:27 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+UPSTREAM_URL = "https://your-domain.com/index.php"
+REQUEST_TIMEOUT = 8
+
+# Если хочешь, можешь ограничить bundle ids прямо тут
+ALLOWED_BUNDLES = {
+    "com.yourcompany.yourapp",
+}
+
+# Можно проверять минимальную версию, а можно убрать
+MIN_APP_VERSION = "1.0.0"
